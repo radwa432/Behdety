e4faf3813e57958a5334c181ca45d5dedaa24930
Rachel Saey <ns1662@fayoum.edu.eg>
2025-05-02 13:50:54 -0700

    commit msg

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -15,10 +15,8 @@ import { DriversDashboardComponent } from './pages/drivers-dashboard/drivers-das
 import { ViewpostDashboardComponent } from './pages/viewpost-dashboard/viewpost-dashboard.component';
 import { AuthorDashboardComponent } from './pages/author-dashboard/author-dashboard.component';
 import { AuthorPostsComponent } from './pages/author-posts/author-posts.component';
-
 import { AuthorpostdahboardComponent } from './pages/authorposts-dashboard/authorposts.component';
 import { GovernmentsComponent } from './pages/governments-dashboard/governments-dashboard.component';
-
 import { BookingConfirmationComponent } from './booking-confirmation/booking-confirmation.component';
 import { AdminHomeComponent } from './admin-home/admin-home.component';
 import { AdminAddTripComponent } from './admin-add-trip/admin-add-trip.component';
@@ -27,26 +25,21 @@ import { PaymentComponent } from './payment/payment.component';
 import { BookingCanceledComponent } from './booking-canceled/booking-canceled.component';
 import { TripDashboardComponent } from './pages/trip-dashboard/trip-dashboard.component';
 import { SiteDashboardComponent } from './pages/site-dashboard/site-dashboard.component';
-//import { TripDashboardComponent } from './pages/trip-dashboard/trip-dashboard.component';
-//import { TripDashboardComponent } from './pages/trip-dashboard/trip-dashboard.component';
-
-
 import { BookingDashboardComponent } from './pages/booking-dashboard/booking-dashboard.component';
 import { BookingformDashboardComponent } from './pages/bookingform-dashboard/bookingform-dashboard.component';
+import { SitesByGovernmentComponent } from './components/site-by-government/sites-by-government.component';
+
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'tours', component: TourComponent },
-  { path: 'about', component: AboutComponent },
   { path: 'about', component: AboutComponent },
   { path: 'contact', component: ContactComponent },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignUpComponent },
   { path: 'Overview', component: OverviewComponent },
   { path: 'faqs', component: FaqsComponent },
-  { path: 'Overview', component: OverviewComponent },
-  { path: 'faqs', component: FaqsComponent },
- ///////////
+
   {
     path: 'post/:id',
     loadComponent: () => import('./pages/post-details/post-details.component').then(m => m.PostDetailsComponent)
@@ -63,10 +56,9 @@ export const routes: Routes = [
     path: 'category/:id',
     loadComponent: () => import('./pages/category-posts/category-posts.component').then(m => m.CategoryPostsComponent)
   },
- //
+
   { path: 'payment/:bookingId', component: PaymentComponent },
   { path: 'payment', component: PaymentComponent },
-
 
   {
     path: 'admin-main',
@@ -78,21 +70,29 @@ export const routes: Routes = [
       { path: '', redirectTo: 'home', pathMatch: 'full' }
     ]
   },
+
   {
     path: 'create-post',
     loadComponent: () => import('./pages/postformCreate/post-form.component').then(m => m.CreatePostComponent),
     title: 'Create Post'
   },
   {
+    path: 'edit-post/:id',
+    loadComponent: () => import('./pages/postformCreate/post-form.component').then(m => m.CreatePostComponent),
+    title: 'Edit Post'
+  },
+
+  {
     path: 'booking-confirmation/:id',
     component: BookingConfirmationComponent,
     title: 'Booking Confirmation'
   },
   {
-    path: 'edit-post/:id',
-    loadComponent: () => import('./pages/postformCreate/post-form.component').then(m => m.CreatePostComponent),
-    title: 'Edit Post'
+    path: 'booking-canceled',
+    component: BookingCanceledComponent,
+    title: 'Booking Canceled'
   },
+
   {
     path: 'dashboard/categories',
     component: CategoryDashboardComponent,
@@ -109,11 +109,6 @@ export const routes: Routes = [
     title: 'Drivers Dashboard'
   },
   {
-    path: 'booking-canceled',
-    component: BookingCanceledComponent,
-    title: 'Booking Canceled'
-  },
-  {
     path: 'dashboard/viewpost/:id',
     component: ViewpostDashboardComponent,
     title: 'Post Dashboard'
@@ -125,8 +120,8 @@ export const routes: Routes = [
   },
   {
     path: 'dashboard/viewAuthorposts/:id',
-    component: AuthorPostsComponent,
-    title: 'Author Posts'
+    component: AuthorpostdahboardComponent,
+    title: 'Authorpost Dashboard'
   },
   {
     path: 'dashboard/governments',
@@ -139,173 +134,45 @@ export const routes: Routes = [
     title: 'Trip Dashboard'
   },
   {
+    path: 'dashboard/site',
+    component: SiteDashboardComponent,
+    title: 'Site Dashboard'
+  },
+  {
+    path: 'dashboard/booking',
+    component: BookingDashboardComponent,
+    title: 'Booking Dashboard'
+  },
+  {
+    path: 'dashboard/bookings/create',
+    component: BookingformDashboardComponent,
+    title: 'Create Booking'
+  },
+  {
+    path: 'dashboard/bookings/edit/:id',
+    component: BookingformDashboardComponent,
+    title: 'Edit Booking'
+  },
+
+  {
     path: 'governrate/:id',
     component: GovernmentsComponent
   },
   {
     path: 'government/:id',
     loadComponent: () => import('./components/site-by-government/sites-by-government.component').then(m => m.SitesByGovernmentComponent)
-  }
-<<<<<<< HEAD
-
-=======
-  ,
+  },
   {
-    path: 'dashboard/site',
-    component: SiteDashboardComponent,
-    title: 'Site Dashboard'
-  }
->>>>>>> faa0dc6464799fe1b3deca3604ae1f5c87749945
+    path: 'government/:id',
+    component: SitesByGovernmentComponent  
+  },
+  {
+    path: '',
+    loadComponent: () => import('./home/home.component').then(m => m.HomeComponent),
+  },
+  {
+    path: 'site-by-government/:id',
+    loadComponent: () => import('./components/site-by-government/sites-by-government.component').then(m => m.SitesByGovernmentComponent),
+  },
+  
 ];
-      { path: '', redirectTo: 'home', pathMatch: 'full' },
-      {
-        path: 'dashboard/posts',
-        loadComponent: () => import('./pages/postdashboard/postdashboard.component').then(m => m.DashboardComponent),
-        title: 'Post Dashboard'
-      },
-      
-      {
-        path: 'dashboard/viewpost/:id',
-        component: ViewpostDashboardComponent,
-        title: 'post Dashboard'
-      },
-
-      {
-        path: 'dashboard/categories',
-        component: CategoryDashboardComponent,
-        title: 'Category Dashboard'
-      },
-      {
-        path: 'dashboard/transportations',
-        component: TransportationsComponent,
-        title: 'Transportations Dashboard'
-      },
-    
-      {
-        path: 'dashboard/drivers',
-        component: DriversDashboardComponent,
-        title: 'drivers Dashboard'
-      },
-    
-
-      {
-        path: 'dashboard/authors',
-        component: AuthorDashboardComponent,
-        title: 'Author Dashboard'
-      },
-      {
-        path: 'dashboard/viewAuthorposts/:id',
-        component: AuthorpostdahboardComponent,
-        title: 'Authorpost Dashboard'
-      },
-      {
-        path: 'dashboard/governments',
-        component: GovernmentsComponent,
-        title: 'Governments Dashboard'
-      },
-      { path: 'dashboard/booking',
-         component: BookingDashboardComponent,
-          title: 'Booking Dashboard' },
-
-          {
-            path: 'create-post',
-            loadComponent: () => import('./pages/postformCreate/post-form.component').then(m => m.CreatePostComponent),
-            title: 'Create Post'
-          },
-          {
-            path: 'edit-post/:id',
-            loadComponent: () => import('./pages/postformCreate/post-form.component').then(m => m.CreatePostComponent),
-            title: 'Edit Post'
-          
-          },
-          {
-            path: 'dashboard/viewpost/:id',
-            component: ViewpostDashboardComponent,
-            title: 'post Dashboard'
-          },
-          
-         
-      
-    ]
-  },
-
-  {
-    path: 'booking-confirmation/:id',
-    component: BookingConfirmationComponent,
-    title: 'Booking Confirmation'
-  },
-  
- 
-  
-  {
-    path: 'booking-canceled',
-    component: BookingCanceledComponent,
-    title: 'Booking Canceled'
-  },
- 
-  // {
-  //   path: 'dashboard/trip',
-  //   component: TripDashboardComponent,
-  //   title: 'Trip Dashboard'
-  // }
- 
-  
-
- 
-  //booking confirmation and payment routes
-
-  {
-    path: 'booking-confirmation/:id',
-    component: BookingConfirmationComponent
-  },
-
-  {
-    path: 'booking-canceled',
-    component: BookingCanceledComponent
-  },
-
-  { path: 'payment/:bookingId', component: PaymentComponent },
-
-  { path: 'payment', component: PaymentComponent },
-
-
-// //dashboard route
-
-{
-  path: 'create-post',
-  loadComponent: () => import('./pages/postformCreate/post-form.component').then(m => m.CreatePostComponent),
-  title: 'Create Post'
-},
-{
-  path: 'edit-post/:id',
-  loadComponent: () => import('./pages/postformCreate/post-form.component').then(m => m.CreatePostComponent),
-  title: 'Edit Post'
-
-},
-{
-  path: 'dashboard/viewpost/:id',
-  component: ViewpostDashboardComponent,
-  title: 'post Dashboard'
-},
-
-{
-  path: 'dashboard/viewAuthorposts/:id',
-  component: AuthorpostdahboardComponent,
-  title: 'Authorpost Dashboard'
-},
-
-{
-  path: 'dashboard/bookings/create',
-  component: BookingformDashboardComponent,
-  title: 'Create Booking'
-},
-{
-  path: 'dashboard/bookings/edit/:id',
-  component: BookingformDashboardComponent,
-  title: 'Edit Booking'
-}
-];
-
-
-
-
