9243c4c583b212483e8ad2b5ada53f7d4fd85b9e
Rachel Saey <ns1662@fayoum.edu.eg>
2025-05-03 04:06:29 -0700

    commit msg

--- src/app/components/sign-up/sign-up.component.ts
+++ src/app/components/sign-up/sign-up.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
+
 @Component({
   selector: 'app-sign-up',
   imports: [ReactiveFormsModule,RouterLink],
